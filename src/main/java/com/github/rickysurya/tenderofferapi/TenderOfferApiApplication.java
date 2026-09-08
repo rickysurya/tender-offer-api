d56147c2a63d7122d363f7dae4c247d5d881bcc1
rickysurya <rickysuryaa10@gmail.com>
2026-09-08 20:27:34 +0700

    initial commit

---
+++ src/main/java/com/github/rickysurya/tenderofferapi/TenderOfferApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rickysurya.tenderofferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderOfferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderOfferApiApplication.class, args);
+	}
+
+}
