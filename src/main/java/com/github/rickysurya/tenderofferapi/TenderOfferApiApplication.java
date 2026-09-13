d68fcbbf9822fc0dd9bac8b145eab3b6237e8e40
rickysurya <rickysuryaa10@gmail.com>
2026-09-13 23:36:27 +0700

    chore: add content.json as storage location and add enable scheduling annotation

--- src/main/java/com/github/rickysurya/tenderofferapi/TenderOfferApiApplication.java
+++ src/main/java/com/github/rickysurya/tenderofferapi/TenderOfferApiApplication.java
@@ -3,8 +3,10 @@ package com.github.rickysurya.tenderofferapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TenderOfferApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/rickysurya/tenderofferapi/service/DisplayService.java
+++
@@ -1,4 +0,0 @@
-package com.github.rickysurya.tenderofferapi.service;
-
-public class DisplayService {
-}
